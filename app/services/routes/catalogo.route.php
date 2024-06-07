e5fec2ff530e0e942c9c115e5b21e6d298aa2f8c
Miguel <miguelagustin182@gmail.com>
2024-06-06 22:22:55 -0600

    fix: catalogo peticion y el home le quite la llave _ap

--- app/services/routes/catalogo.route.php
+++ app/services/routes/catalogo.route.php
@@ -1,22 +1,18 @@
 <?php
 
 namespace app;
-//catalogo.route.php
 require_once "../../autoloader.php";
 
 use Controllers\CarritoController;
 use Controllers\Middleware;
 use Controllers\PostController as PostController;
 
-//use app\PasswordHash;
 require('../../libraries/phpass-master/PasswordHash.php');
 
 try {
     $pwdHasher = new PasswordHash(8, FALSE);
-    $autorization = new Middleware();
     $api = new Middleware();
     $_HEADERS = apache_request_headers();
-    $route = $_SERVER['REQUEST_URI'];
     $contentCatalogo = $api->autorization($_HEADERS, $pwdHasher);
 
     if (!$contentCatalogo) {
@@ -24,58 +20,47 @@ try {
         echo json_encode($json, JSON_PRETTY_PRINT);
         return;
     }
+    $userId = $contentCatalogo['userId'];
 
     //metodo para agregar un producto al carrito del cliente
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['_ap'])) {
-        //print_r('recibido');
         $productId = $_POST['pid'];
         $cantidad = $_POST['cantidad'];
-        $verificar = $api->autorization($_HEADERS, $pwdHasher);
-        if ($verificar['success']) {
-            $userId = $verificar['userId'];
-            $carrito = new CarritoController();
-            $productoExistente = $carrito->agregarProducto($productId, $userId, $cantidad);
-            $cantidadPr = $carrito->cantProductos($userId);
-            //print_r("Llego aqui la peticion de anadir");
-            $json["response"] = 1;
-            echo json_encode($json, JSON_PRETTY_PRINT);
-            return;
-        }
-        $json["response"] = "failed";
+        $carrito = new CarritoController();
+        $productoExistente = $carrito->agregarProducto($productId, $userId, $cantidad);
+        $cantidadPr = $carrito->cantProductos($userId);
+        $json["response"] = 1;
         echo json_encode($json, JSON_PRETTY_PRINT);
         return;
     }
 
-    //metodo para traer los productos al catalogo 
+    // //metodo para traer los productos al catalogo 
     $tp = in_array('_tp', array_keys(filter_input_array(INPUT_GET)));
     if ($tp) {
-        $postController = new PostController();
+        $productos = new PostController();
         $type = filter_input(INPUT_GET, '_tp');
-        $result = $postController->getProductsToClient($type);
+        $result = $productos->getProductsToClient($type);
         if (!$result) {
             $json["response"] = "internal server error or auth denied";
             echo json_encode($json, JSON_PRETTY_PRINT);
             return;
         }
         print_r($result);
+        return;
     }
 
     //metodo para mostrar la cantidad de productos en el carrito
     $np = in_array('_np', array_keys(filter_input_array(INPUT_GET)));
     if ($np) {
-        $verificar = $api->autorization($_HEADERS, $pwdHasher);
-        if ($verificar['success']) {
-            $userId = $verificar['userId'];
-
-            $carrito = new CarritoController();
-            $cantidadPr = $carrito->cantProductos($userId);
-            $json["response"] = $cantidadPr; // donde $cantidadPr es el retorno de cantProductos()
-            echo json_encode($json);            
-            return;
-        } else {
-            echo json_encode(["response" => "Unauthorized"]);
+        $carrito = new CarritoController();
+        $result = $carrito->cantProductos($userId);
+        if (!$result) {
+            $json["response"] = "internal server error or auth denied";
+            echo json_encode($json, JSON_PRETTY_PRINT);
             return;
         }
+        print_r($result);
+        return;
     }
 } catch (Exception $e) {
     $json["response"] = "internal server error";
--- app/services/routes/home.route.php
+++ app/services/routes/home.route.php
@@ -12,46 +12,40 @@ require('../../libraries/phpass-master/PasswordHash.php');
 
 try {
     $pwdHasher = new PasswordHash(8, FALSE);
-    $autorization = new Middleware();
     $api = new Middleware();
     $_HEADERS = apache_request_headers();
     $contentHome = $api->autorization($_HEADERS, $pwdHasher);
+
     if(!$contentHome){
         $json["response"] = "No hay contenido que mostrarte";
         echo json_encode($json, JSON_PRETTY_PRINT);
         return;
     }
-
+    $userId = $contentHome['userId'];
     //metodo para agregar un producto al carrito del cliente
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['_ap'])) {
         $productId = $_POST['pid'];
         $cantidad = $_POST['cantidad'];
-        $verificar = $api->autorization($_HEADERS, $pwdHasher);
-        if ($verificar['success']) {
-            $userId = $verificar['userId'];
-            $carrito = new CarritoController();
-            $productoExistente = $carrito->agregarProducto($productId, $userId, $cantidad);
-            $cantidadPr = $carrito->cantProductos($userId);
-            //print_r("Llego aqui la peticion de anadir");
-            $json["response"] = 1;
-            echo json_encode($json, JSON_PRETTY_PRINT);
-            return;
-        }
-        $json["response"] = "failed";
+        $carrito = new CarritoController();
+        $productoExistente = $carrito->agregarProducto($productId, $userId, $cantidad);
+        $cantidadPr = $carrito->cantProductos($userId);
+        $json["response"] = 1;
         echo json_encode($json, JSON_PRETTY_PRINT);
         return;
     }
 
     //metodo para traer los productos al catalogo de muestra en home
     $lp = in_array('_lp', array_keys(filter_input_array(INPUT_GET)));
-    $productos = new PostController();
-    $result = $productos->getProductsToHome(4);
-    if(!$result){
-        $json["response"] = "internal server error or auth denied";
-        echo json_encode($json, JSON_PRETTY_PRINT);
-        return;
+    if($lp){
+        $productos = new PostController();
+        $result = $productos->getProductsToHome(4);
+        if(!$result){
+            $json["response"] = "internal server error or auth denied";
+            echo json_encode($json, JSON_PRETTY_PRINT);
+            return;
+        }
+        print_r($result);
     }
-    print_r($result);
 
 
 } catch(Exception $e) {
